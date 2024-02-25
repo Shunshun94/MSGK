fd7989f7661131c7235ee4f5947dc39859d03d7e
Shunshun94 <shunshun9460@gmail.com>
2024-02-25 21:50:40 +0900

    add save button

--- app/v1/js/app.js
+++ app/v1/js/app.js
@@ -2,9 +2,20 @@ var MSGK = MSGK || {};
 MSGK.APP = MSGK.APP || {};
 
 MSGK.INPUT.initialize();
+MSGK.OUTPUT.initialize();
 
 document.getElementsByClassName('input')[0].addEventListener(MSGK.INPUT.CONSTS.UPDATE_EVENT, (e)=>{
     const cards = MSGK.INPUT.getCardInfo();
     MSGK.OUTPUT.buildOutput(cards);
+});
+
+document.getElementsByClassName('output')[0].addEventListener(MSGK.OUTPUT.CONSTS.SAVE_EVENT, (e)=>{
+    html2canvas(document.getElementsByClassName('output')[0]).then(canvas => {
+        let downloadEle = document.createElement("a");
+        downloadEle.href = canvas.toDataURL("image/png");
+        downloadEle.download = document.getElementById(`magicContent_${number}`).title + '.png';
+        downloadEle.click();
+        downloadEle.remove();
+    });
 });
 
--- app/v1/js/output.js
+++ app/v1/js/output.js
@@ -5,9 +5,13 @@ MSGK.OUTPUT.DOMS = {
     BASE: document.getElementsByClassName('output')[0],
 };
 
+MSGK.OUTPUT.CONSTS = {
+    SAVE_EVENT: 'MSGK_OUTPUT_SAVE',
+};
 
 MSGK.OUTPUT.buildOutput = (cards) => {
-    MSGK.OUTPUT.DOMS.BASE.innerHTML = '';
+    const base = document.getElementsByClassName('output-cards')[0];
+    base.innerHTML = '';
     cards.forEach((card)=>{
         const cardBase = document.createElement('div');
         cardBase.className = `card ${card.okng}-card`;
@@ -20,6 +24,22 @@ MSGK.OUTPUT.buildOutput = (cards) => {
         const span = document.createElement('span');
         span.className = 'stump';
         cardBase.appendChild(span);
-        MSGK.OUTPUT.DOMS.BASE.appendChild(cardBase);
+        base.appendChild(cardBase);
+    });
+};
+
+MSGK.OUTPUT.initialize = () => {
+    const cards = document.createElement('div');
+    cards.className = `output-cards`;
+    MSGK.OUTPUT.DOMS.BASE.append(cards);
+    const save = document.createElement('button');
+    save.textContent = '画像として保存';
+    save.className = `output-save`;
+    MSGK.OUTPUT.DOMS.BASE.append(save);
+    save.addEventListener('click', (e)=>{
+        const event = new Event(MSGK.OUTPUT.CONSTS.SAVE_EVENT, {
+            bubbles: true, cancelable: true,
+        });
+        save.dispatchEvent(event);
     });
 };
